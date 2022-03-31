6639d2f0209391042368a6d9368e77d0094d66ba
Natã Elienai <nata.elienai@gmail.com>
2022-03-31 15:55:59 -0300

    test: add tests for ProductModel.getAll

---
+++ test/unit/models/product.model.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+const { expect } = require('chai');
+const sinon = require('sinon');
+const connection = require('../../../models/connection');
+const ProductModel = require('../../../models/product.model');
+
+describe('ProductModel', () => {
+  describe('#getAll()', () => {
+    context('when the database is empty', () => {
+      before(() => {
+        sinon.stub(connection, 'execute').resolves([[]]);
+      });
+
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('returns an empty array', async () => {
+        const products = await ProductModel.getAll();
+        expect(products).to.be.an('array').that.is.empty;
+      });
+    });
+
+    context('when the database has a single product', () => {
+      const singleProduct = [{ id: 1, name: 'Martelo', quantity: 10 }];
+
+      before(() => {
+        sinon.stub(connection, 'execute').resolves([singleProduct]);
+      });
+
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('returns an array with one product', async () => {
+        const products = await ProductModel.getAll();
+        expect(products).to.deep.equal(singleProduct);
+      });
+    });
+
+    context('when the database has multiple products', () => {
+      const multipleProducts = [
+        { id: 1, name: 'Martelo', quantity: 10 },
+        { id: 2, name: 'Prego', quantity: 20 },
+        { id: 3, name: 'Parafuso', quantity: 30 },
+      ];
+
+      before(() => {
+        sinon.stub(connection, 'execute').resolves([multipleProducts]);
+      });
+
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('returns an array with multiple products', async () => {
+        const products = await ProductModel.getAll();
+        expect(products).to.deep.equal(multipleProducts);
+      });
+    });
+  });
+});
