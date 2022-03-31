cbf83ce5fae0fa67318f1df573ff08af9a010345
Natã Elienai <nata.elienai@gmail.com>
2022-03-31 20:15:01 -0300

    feat: implement the ProductModel.getById function

--- models/product.model.js
+++ models/product.model.js
@@ -5,6 +5,16 @@ const getAll = async () => {
   return products;
 };
 
+const getById = async (id) => {
+  const [products] = await connection.execute(
+    'SELECT * FROM StoreManager.products WHERE id = ?',
+    [id],
+  );
+  if (products.length === 0) return null;
+  return products[0];
+};
+
 module.exports = {
   getAll,
+  getById,
 };
