c4e6b650f9bd3bf294ce9c313150abc1525e1480
Natã Elienai <nata.elienai@gmail.com>
2022-03-31 19:27:30 -0300

    feat: add the product router to the express app

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 require('dotenv').config();
+const express = require('express');
+const ProductRouter = require('./routers/product.router');
+
+const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use(ProductRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
