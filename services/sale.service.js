25d2f4f0f171c1ab98718cac9db7ab7714be5975
Natã Elienai <nata.elienai@gmail.com>
2022-04-01 20:32:57 -0300

    feat: implement the SaleService.getById function

--- services/sale.service.js
+++ services/sale.service.js
@@ -1,7 +1,20 @@
 const SaleModel = require('../models/sale.model');
+const errorCodes = require('./errorCodes');
 
 const getAll = async () => ({ data: await SaleModel.getAll() });
 
+const getById = async (id) => {
+  const sale = await SaleModel.getById(id);
+
+  if (!sale) {
+    return {
+      error: { code: errorCodes.NOT_FOUND, message: 'Sale not found' },
+    };
+  }
+  return { data: sale };
+};
+
 module.exports = {
   getAll,
+  getById,
 };
