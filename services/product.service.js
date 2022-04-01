816b37b7a898ef233846910416b8b2634f0403e8
Natã Elienai <nata.elienai@gmail.com>
2022-04-01 12:05:12 -0300

    feat: implement the ProductService.getById function

--- services/product.service.js
+++ services/product.service.js
@@ -1,7 +1,20 @@
 const ProductModel = require('../models/product.model');
+const errorCodes = require('./errorCodes');
 
 const getAll = async () => ProductModel.getAll();
 
+const getById = async (id) => {
+  const product = await ProductModel.getById(id);
+
+  if (!product) {
+    return {
+      error: { code: errorCodes.NOT_FOUND, message: 'Product not found' },
+    };
+  }
+  return product;
+};
+
 module.exports = {
   getAll,
+  getById,
 };
