d980d790d3c6093457a50a084f4518503d28db3f
Natã Elienai <nata.elienai@gmail.com>
2022-04-01 17:37:50 -0300

    feat: add sort by id in the query in ProductModel.getAll()

--- models/product.model.js
+++ models/product.model.js
@@ -1,7 +1,9 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const [products] = await connection.execute('SELECT * FROM StoreManager.products');
+  const [products] = await connection.execute(
+    'SELECT * FROM StoreManager.products ORDER BY id',
+  );
   return products;
 };
 
