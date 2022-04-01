54b1c3aa0ca5bc6122ab7789fd65ffa0c6b396db
Natã Elienai <nata.elienai@gmail.com>
2022-04-01 18:58:57 -0300

    feat: implement the SaleModel.getById function

--- models/sale.model.js
+++ models/sale.model.js
@@ -7,6 +7,12 @@ const serialize = (sale) => ({
   quantity: sale.quantity,
 });
 
+const serializeWithoutSaleId = (sale) => ({
+  productId: sale.product_id,
+  date: sale.date,
+  quantity: sale.quantity,
+});
+
 const getAll = async () => {
   const [sales] = await connection.execute(
     `SELECT sp.sale_id, sp.product_id, s.date, sp.quantity
@@ -18,6 +24,22 @@ const getAll = async () => {
   return sales.map(serialize);
 };
 
+const getById = async (id) => {
+  const [sales] = await connection.execute(
+    `SELECT sp.product_id, s.date, sp.quantity
+      FROM StoreManager.sales s
+      INNER JOIN StoreManager.sales_products sp
+      ON s.id = sp.sale_id
+      WHERE sp.sale_id = ?
+      ORDER BY sp.product_id
+      `,
+    [id],
+  );
+  if (sales.length === 0) return null;
+  return sales.map(serializeWithoutSaleId);
+};
+
 module.exports = {
   getAll,
+  getById,
 };
