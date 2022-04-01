97cd6aac2f0f70a3dd81dcb2e2f20099876ed9df
Natã Elienai <nata.elienai@gmail.com>
2022-04-01 13:16:30 -0300

    feat: implement the ProductController.getById function

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -5,6 +5,17 @@ const getAll = async (req, res) => {
   res.status(200).json(products);
 };
 
+const getById = async (req, res, next) => {
+  const { id } = req.params;
+  const product = await ProductService.getById(id);
+
+  if (product.error) {
+    return next(product.error);
+  }
+  res.status(200).json(product);
+};
+
 module.exports = {
   getAll,
+  getById,
 };
