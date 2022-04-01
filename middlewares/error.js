5b033333c86aa31bc2f11bed15d2444b4ef1cd9e
Natã Elienai <nata.elienai@gmail.com>
2022-04-01 13:30:33 -0300

    feat: implement the error middleware

---
+++ middlewares/error.js
@@ -0,0 +1,14 @@
+const errorCodes = require('../services/errorCodes');
+
+module.exports = (err, _req, res, _next) => {
+  const statusCodeByError = {
+    [errorCodes.NOT_FOUND]: 404,
+  };
+
+  const statusCode = statusCodeByError[err.code] || 500;
+
+  if (statusCode === 500) {
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+  res.status(statusCode).json({ message: err.message });
+};
