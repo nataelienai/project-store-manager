2cc0a8476829dc2c284f3e294e978e5eeae3af43
Natã Elienai <nata.elienai@gmail.com>
2022-04-01 20:32:47 -0300

    test: add tests for the SaleService.getById function

--- test/unit/services/sale.service.js
+++ test/unit/services/sale.service.js
@@ -3,6 +3,8 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 const SaleModel = require('../../../models/sale.model');
 const SaleService = require('../../../services/sale.service');
+const errorCodes = require('../../../services/errorCodes');
+const saleMock = require('../mocks/saleCamelCase.json');
 const salesMock = require('../mocks/salesCamelCase.json');
 
 describe('SaleService', () => {
@@ -51,6 +53,43 @@ describe('SaleService', () => {
       it('returns an array with multiple sales', async () => {
         const { data: sales } = await SaleService.getAll();
         expect(sales).to.deep.equal(salesMock);
+      });
+    });
+  });
+
+  describe('#getById()', () => {
+    context('when the sale is not present', () => {
+      before(() => {
+        sinon.stub(SaleModel, 'getById').resolves(null);
+      });
+
+      after(() => {
+        SaleModel.getById.restore();
+      });
+
+      it(`returns an error object with the code ${errorCodes.NOT_FOUND}`, async () => {
+        const { error } = await SaleService.getById(1);
+        expect(error).to.have.property('code', errorCodes.NOT_FOUND);
+      });
+
+      it('returns an error object with the message "Sale not found"', async () => {
+        const { error } = await SaleService.getById(1);
+        expect(error).to.have.property('message', 'Sale not found');
+      });
+    });
+
+    context('when the sale is present', () => {
+      before(() => {
+        sinon.stub(SaleModel, 'getById').resolves(saleMock);
+      });
+
+      after(() => {
+        SaleModel.getById.restore();
+      });
+
+      it('returns the sale', async () => {
+        const { data: sale } = await SaleService.getById(1);
+        expect(sale).to.deep.equal(saleMock);
       });
     });
   });
