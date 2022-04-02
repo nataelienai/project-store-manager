47f25185b4fb7f2637dff6515dde664601f09250
Natã Elienai <nata.elienai@gmail.com>
2022-04-01 21:09:58 -0300

    feat: implement the SaleController.getById function

--- controllers/sale.controller.js
+++ controllers/sale.controller.js
@@ -5,6 +5,17 @@ const getAll = async (req, res) => {
   res.status(200).json(data);
 };
 
+const getById = async (req, res, next) => {
+  const { id } = req.params;
+  const { error, data } = await SaleService.getById(id);
+
+  if (error) {
+    return next(error);
+  }
+  res.status(200).json(data);
+};
+
 module.exports = {
   getAll,
+  getById,
 };
