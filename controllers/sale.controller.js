0ea398266b200ee892a2dc9b14af503fd1c2e9c5
Natã Elienai <nata.elienai@gmail.com>
2022-04-03 18:57:36 -0300

    feat: implement the SaleController.update function

--- controllers/sale.controller.js
+++ controllers/sale.controller.js
@@ -20,8 +20,14 @@ const create = async (req, res) => {
   res.status(201).json(data);
 };
 
+const update = async (req, res) => {
+  const { data } = await SaleService.update(req.params.id, req.body);
+  res.status(200).json(data);
+};
+
 module.exports = {
   getAll,
   getById,
   create,
+  update,
 };
